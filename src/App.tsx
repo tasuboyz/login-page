037c10103f3e24ba18f2d82742f737ee14148f08
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-09-01 12:23:11 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Telegram } from "@twa-dev/types";
 import { postAPI } from './api/http_request';
